6a835a91dcd3f072093530985c0f1566a3e597e7
Emily Read <emilyread01@gmail.com>
2021-06-28 11:38:17 -0700

    added bonus

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function logout(element) {
 function hide(element){
     element.remove()
 }
+
+function like(element){
+    var word= element.parentNode.childNodes[1].innerText;
+    alert(word + " was clicked")
+}
+
